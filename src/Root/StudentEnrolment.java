60bf35d2ae57c1fad72b1fb82f220660aa0efbaf
ADMIN <s3755063@rmit.edu.vn>
2021-04-08 08:10:01 +0700

    Student enrollment class updated

--- src/Root/StudentEnrolment.java
+++ src/Root/StudentEnrolment.java
@@ -1,4 +1,19 @@
 package Root;
 
 public class StudentEnrolment {
+    private Student student;
+    private Course Course;
+    private String semester;
+
+    public StudentEnrolment(String semester) {
+        this.semester = semester;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
 }
