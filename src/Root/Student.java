fd2d5ffb2140408e4e270c2f1b8619f5f15ade46
ADMIN <s3755063@rmit.edu.vn>
2021-04-08 07:56:25 +0700

    Updated Student class

--- src/Root/Student.java
+++ src/Root/Student.java
@@ -1,4 +1,39 @@
 package Root;
 
+import java.util.Date;
+
 public class Student {
+    private String sid;
+    private String name;
+    private Date birthdate;
+
+    public Student(String sid, String name, Date birthdate) {
+        this.sid = sid;
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
 }
